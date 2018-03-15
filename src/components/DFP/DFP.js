d52766e2a6486e039743e4d134814ddecf91fc08
zeroxys <miguelzavalac@gmail.com>
2018-03-15 12:00:57 -0600

    agregando dfp

--- src/components/DFP/DFP.js
+++ src/components/DFP/DFP.js
@@ -5,7 +5,6 @@ const DFP = () => {
   return (
     <DFPSlotsProvider dfpNetworkId={'35139216'} adUnit={"Actitudfem"}>
       <AdSlot sizes={[ [900, 90], [728, 90]]} />
-      <AdSlot adUnit={"home/mobile"} sizes={[ [300, 250], [300, 600]]}/>
     </DFPSlotsProvider>
   )
 }
