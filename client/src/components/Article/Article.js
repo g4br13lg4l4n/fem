80adaea33257a22406342f1e46949e63d1dbfe39
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel.local>
2018-03-16 12:17:26 -0600

    merge

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -41,15 +41,9 @@ const styles = {
 
   return (
     <article>
-<<<<<<< HEAD:src/components/Article/Article.js
-      <img style={styles.article.img} alt={'adsad'} src={'http://cdn2.actitudfem.com/media/files/styles/medium/public/images/2018/03/lissette-calveiro-instagram-influences-que-vivio-una-mentira.jpg'}/>
-      <h3 style={styles.article.header}>Lissette Calveiro cuenta cómo vivir la mentira de ser influencer en Instagram la endeudó</h3>
-      <p style={styles.article.body}>Cuando Lissette Calveiro se mudó a Nueva York, pensó que estaba viviendo como una de las chicas de Sex and the City.&nbsp;Ropa y zapatos de marca, brunches con sus amigas, definitivamente estaba llevando la vida de Carrie Bradshaw y no podía dejar de presumirla en...</p>
-=======
       <img alt={'adsad'} src={props.img}/>
       <h3 style={styles.article.header}>{props.title}</h3>
       <p style={styles.article.body}>{props.summary}</p>
->>>>>>> 7a5fac82b22cd16b6341c5d1dd86e93aba682198:client/src/components/Article/Article.js
       <div style={styles.article.blur}>
         <button style={styles.article.seeMore}>VER MÁS</button>
       </div>  
